6da77bc0692ee301335b199c7d8df3e069572d74
aslaameen <fathimaasla2005@gmail.com>
2026-02-10 15:38:25 +0530

    garage_hub

--- GARAGE_APP/urls.py
+++ GARAGE_APP/urls.py
@@ -1,3 +1,13 @@
+from django.urls import path
+
+from GARAGE_APP import views
+
 urlpatterns = [
+     path("index",views.index,name="index"),
+     path("admin",views.admin,name="admin"),
+     path("service_center",views.service_center,name="service_center"),
+     path("customer",views.customer,name="customer"),
+     path("login_view",views.login_view,name="login_view"),
+
 
 ]
--- GARAGE_APP/views.py
+++ GARAGE_APP/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
+
+
+def index(request):
+    return render (request,'index.html')
+
+
+
+
+#        base views
+# ---------------------------
+def admin(request):
+    return render(request,'admin/adminbase.html')
+
+def service_center(request):
+    return render(request,'service_center/service_centerbase.html')
+
+def customer(request):
+    return render(request,'customer/customerbase.html')
+
+#        base views
+# ---------------------------
+
+def login_view(request):
+    if request.method == "POST":
+         username = request.POST.get('uname')
+         password = request.POST.get('pass')
+         user = authenticate(request, username=username, password=password)
+         # authenticate= inbuild function aan
+         if user is not None:
+            login(request,user)
+            if user.is_staff:
+                return redirect ('admin')
+            elif user.is_customer:
+                return redirect('service_center')
+            elif user.is_servicecenter:
+                return redirect('customer')
+         else:
+             messages.info(request, 'Username or password is incorrect')
+    return render(request, 'login.html')
